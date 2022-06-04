d5720cca7a2affa64c9692c13649984556d4d074
Flatl1neZ <ZhengJiang_Ajax@Hotmail.com>
2022-06-04 04:00:16 -0300

    100%

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const { Person } = require("./person");
+// const { Person } = require("./person");
+
+const dotenv = require("dotenv");
+const connectToDatabase = require("./src/databse/connect");
+
+dotenv.config();
+
+connectToDatabase();
 
 //require("./modules/path");
 //require("./modules/fs");
-require("./modules/http");
+// require("./modules/http");
 
-const person = new Person("Lucas");
+require("./modules/express");
+
+// const person = new Person("Lucas");
--- modules/http.js
+++ modules/http.js
@@ -8,17 +8,6 @@ const server = http.createServer((req, res) => {
     return res.end("<h1>home page</h1>");
   }
   if (req.url === "/users") {
-    const users = [
-      {
-        name: "John Doe",
-        email: "john@doe.com",
-      },
-      {
-        name: "Jane Doe",
-        email: "jane@doe.com",
-      },
-    ];
-
     res.writeHead(200, { "Content-Type": "application/json" });
     return res.end(JSON.stringify(users));
   }
